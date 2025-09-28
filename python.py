086dee50c9226fd2fb8118509ddde199ab3743c1
Olivier Dore <olivierdore3@gmail.com>
2025-09-28 16:09:17 +0100

    feat: Changed File

--- python.py
+++ python.py
@@ -1,1 +1,2 @@
-print("This is the beginning of my Python Project")
+print("This is the beginning of my Python Project - but I just added this line to create a conflict")
+print('Nous allons créer un conflit avec notre branch main, En effet on a modifié le fichier python.py et surtout la ligne 1')
