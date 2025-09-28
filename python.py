ab03ef5e3c841f733d2af8974e34e04647674826
Olivier Dore <olivierdore3@gmail.com>
2025-09-28 16:16:42 +0100

    feat: I Change My Code

--- python.py
+++ python.py
@@ -1,2 +1,2 @@
-print("This is the beginning of my Python Project - but I just added this line to create a conflict")
-print('Nous allons créer un conflit avec notre branch main, En effet on a modifié le fichier python.py et surtout la ligne 1')
+print("I changed this line on purpose to create a conflict")
+
